356bf925a1dd76b3f894e2084b0e8b598ebbe4c6
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-18 11:47:22 -0300

    Add get posts by user id controller

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -25,4 +25,18 @@ const getPosts = async (req, res) => {
   }
 };
 
-module.exports = { createPost, getPosts };
+const getPostsByUserId = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const data = await Posts.getPostsByUserId(_id);
+    if (data.err) {
+      return res.status(data.err.code).json(data.err.message); 
+    }
+     return res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong. Try again later' });
+  }
+};
+
+module.exports = { createPost, getPosts, getPostsByUserId };
