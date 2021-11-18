7a6ca0f58982dc7fb9f288e0b4df308cf2831124
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-17 21:35:38 -0300

    Creates app.js and change index.js

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-require('dotenv/config');
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = require('./src/app');
 
 const port = process.env.PORT || 8080;
-
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
