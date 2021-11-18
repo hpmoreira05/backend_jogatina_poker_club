4b63780215654bf5a668283268b5d96d1665d317
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-17 22:01:30 -0300

    Create users routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
+const usersRoutes = require('./routes/usersRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello!');
