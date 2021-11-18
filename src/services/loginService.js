e8f2bc3cf55878019a4bcb0f919a08eec9d033db
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-17 22:51:48 -0300

    Add login service and token generator

---
+++ src/services/loginService.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const { ObjectId } = require('mongodb');
+const User = require('../models/usersModel');
+
+const generateToken = (user, email) => {
+  const secret = process.env.SECRET_KEY;
+  const jwtConfig = {
+    expiresIn: '8h',
+    algorithm: 'HS256',
+  };
+  const { _id } = user;
+  const userId = ObjectId(_id).toString();
+  const userInfoToken = { userId, email };
+  const token = jwt.sign({ data: userInfoToken }, secret, jwtConfig);
+  return { token };
+};
+
+const login = async ({ email, password }) => {
+  if (!email || !password) {
+    return { err: { code: 401, message: { message: 'All fields must be filled' } } };
+  }
+  const user = await User.getEmail(email);
+  if (!user || user.password !== password) {
+    return { err: { code: 401, message: { message: 'Incorrect username or password' } } };
+  }
+  generateToken(user, email);
+};
+
+module.exports = { login };
