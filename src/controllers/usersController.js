52b1257ac062338dee176cd57681a45bede8d909
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-17 22:20:34 -0300

    Add register user validations

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -7,7 +7,7 @@ const createUser = async (req, res) => {
     if (data.err) {
       return res.status(data.err.code).json(data.err.message); 
     }
-     return res.status(201).json(data);
+    return res.status(201).json(data);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Something went wrong. Try again later' });
--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,6 +1,17 @@
 const Users = require('../models/usersModel');
 
+const verifyUserInfo = async (email, password, name) => {
+  const validEmail = /\S+@\S+\.\S+/;
+  if (!email || !name || !password) {
+    return { err: { code: 400, message: { message: 'Invalid entries. Try again.' } } };
+  }
+  if (!validEmail.test(email)) {
+    return { err: { code: 400, message: { message: 'Invalid email format. Try again.' } } };
+  }
+};
+
 const createUser = async ({ email, password, name }) => {
+  if (await verifyUserInfo(email, password, name)) return verifyUserInfo(email, password, name);
   const registeredUser = await Users.createUser({ email, password, name });
   return registeredUser;
 };
