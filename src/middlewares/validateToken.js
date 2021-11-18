44d9f0a1c7268b848c4a7e4a0805a0bb586d5f19
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-17 23:11:59 -0300

    Add middleware to validate token

---
+++ src/middlewares/validateToken.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const Users = require('../models/usersModel');
+
+const secret = process.env.SECRET_KEY;
+
+module.exports = async (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Missing auth token' });
+  }
+  try {
+    const decoded = jwt.verify(token, secret);
+    const user = await Users.getUserByEmail(decoded.data.email);
+    console.log(user);
+    if (!user) {
+      return res.status(401).json({ message: 'JWT malformed' });
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'JWT malformed' });
+  }
+};
