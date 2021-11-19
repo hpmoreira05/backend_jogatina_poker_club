b88959bf14f1a3368edd057a475116d3ddde55ee
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-18 23:20:07 -0300

    Add send email service

---
+++ src/services/sendEmailService.js
@@ -0,0 +1,26 @@
+require('dotenv/config');
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (email, name) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASSWORD, 
+      },
+    });
+
+    await transporter.sendMail({
+      from: '"Talk2Me Support" <talk2me.noreply@gmail.com>', 
+      to: email, 
+      subject: 'Account Created Successfully ✔', 
+      text: `Hello ${name}! Your account has been created! Welcome, and enjoy ;)`,
+    });
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+module.exports = { sendEmail };
--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,4 +1,5 @@
 const Users = require('../models/usersModel');
+const { sendEmail } = require('./sendEmailService');
 
 const verifyUserInfo = async (email, password, name) => {
   const validEmail = /\S+@\S+\.\S+/;
@@ -14,6 +15,10 @@ const createUser = async ({ email, password, name }) => {
   if (await verifyUserInfo(email, password, name)) return verifyUserInfo(email, password, name);
   const emailExists = await Users.getUserByEmail(email);
   if (emailExists) return { err: { code: 409, message: { message: 'Email already registered' } } };
+  const emailSent = await sendEmail(email, name);
+  if (!emailSent) {
+    return { err: { code: 500, message: { message: 'Something went wrong. Try again later' } } };
+  }
   const registeredUser = await Users.createUser({ email, password, name });
   return registeredUser;
 };
